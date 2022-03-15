8c145cb83481d47e050e7bc8f13c3f37af62e5ec
c <caio.asevedo@gmail.com>
2022-03-15 13:20:32 -0300

    result page added

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -4,6 +4,8 @@ require "open-uri"
 
 class ResultsController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
+  before_action :set_params
+
 
   def index
     set_request
@@ -19,11 +21,10 @@ class ResultsController < ApplicationController
 
   def set_request
     # o site tá quebrando se não há query alguma inserida na barra de pesquisa e o usuário clica search "NoMethodError in Results#index"
-    set_params
     if params[:query].present?
       results = Geocoder.search(params[:query])
       @latlong = "#{results.first.coordinates[0]},#{results.first.coordinates[1]}"
-      url = URI("https://api.foursquare.com/v3/places/search?query=#{@query}&ll=#{@latlong}&radius=#{@radius}&categories=#{@categories}&exclude_all_chains=#{@exclude_chains}&fields=name%2Cgeocodes%2Cdistance%2Cdescription%2Ctel%2Cwebsite%2Csocial_media%2Crating%2Cprice%2Ctastes%2Clocation&min_price=#{@min_price}&max_price=#{@max_price}&open_now=#{@open_now}&limit=#{@limit}")
+      url = URI("https://api.foursquare.com/v3/places/search?query=#{@tastes}&ll=#{@latlong}&radius=#{@radius}&categories=#{@categories}&exclude_all_chains=#{@exclude_chains}&fields=name%2Cgeocodes%2Cdistance%2Cdescription%2Ctel%2Cwebsite%2Csocial_media%2Crating%2Cprice%2Ctastes%2Clocation&min_price=#{@min_price}&max_price=#{@max_price}&open_now=#{@open_now}&limit=#{@limit}")
       foursquare_request = URI.open(url, "Authorization" => ENV['FOURSQUARE_KEY']).read
       foursquare_response = JSON.parse(foursquare_request)
       @results = foursquare_response['results']
@@ -41,17 +42,18 @@ class ResultsController < ApplicationController
   end
 
   def set_params
-    @query = 'romantic'
+    #@taste_options = ['sushi', 'pizza']
+    @tastes = params[:taste]
     # %20 = espaço, %2C = virgula
     # Tastes detalhados pelo usuário - tacos, good for groups, romantic, sushi, good for late nights, live music.
-    @radius = ''
+    @radius = 5000
     #raio de busca de restaurantes - padrão pode ser 5km
-    @open_now = false
+    @open_now = ''
     #open_now = true(padrão)
-    @latlong = 123
+    @latlong = 0
     @categories = 13000
     #categories = 13000 por padrão (13000 referente a restaurantes e bares)
-    @exclude_chains = false
+    @exclude_chains = true
     #exclude_all_chains - excluir grandes redes de restaurantes (true or false)
     @min_price = 1
     #minprice = valor de 1 até 4
