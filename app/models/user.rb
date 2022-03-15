8cbad27296d65826229f5a2e2012abbb3af28d18
leon-siqueira <leon.siqueir4@gmail.com>
2022-03-15 16:55:29 -0300

    attr_accessible removed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  attr_accessible :name
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :moods
