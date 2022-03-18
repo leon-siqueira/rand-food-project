4ae9994e3b0e50658d2695d012c1683219d74a18
c <caio.asevedo@gmail.com>
2022-03-18 19:32:44 -0300

    teste

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -71,52 +71,50 @@ class ResultsController < ApplicationController
   end
 end
 
+# def set_params
+#   @mood = Mood.find(params[:mood]) if params[:mood].present?
+#   if @mood.nil?
+#     @tastes = ''
+#     @radius = 10000
+#     @min_price = 1
+#     @max_price = 4
+#   else
+#     @tastes = @mood.tastes
+#     @query = @mood.query
+#     @radius = @mood.near
+#     @min_price = @mood.min_price
+#     @max_price = @mood.max_price
+#   end
+#   @limit = 10
+#   @open_now = 'true'
+#   @latlong =
+#   @categories = 13000
+#   @exclude_chains = true
+# end
+# end
 
+# @mood.tastes = "sushi, tacos, burger"
+# @count = @tastes.split.count # 3
+# @limit = 50 / tastes.split.count
 
-def set_params
-  @mood = Mood.find(params[:mood]) if params[:mood].present?
-  if @mood.nil?
-    @tastes = ''
-    @radius = 10000
-    @min_price = 1
-    @max_price = 4
-  else
-    @tastes = @mood.tastes
-    @query = @mood.query
-    @radius = @mood.near
-    @min_price = @mood.min_price
-    @max_price = @mood.max_price
-  end
-  @limit = 10
-  @open_now = 'true'
-  @latlong =
-  @categories = 13000
-  @exclude_chains = true
-end
-end
+# if count == nil?
+# elsif count == 1
+#   @limit = 50
+# elsif count == 2
+#   @limit = 25
+# elsif count == 3
+#   @limit = 20
+# end
 
-@mood.tastes = "sushi, tacos, burger"
-@count = @tastes.split.count # 3
-@limit = 50 / tastes.split.count
+# @resultados_organizados = [] # 60 resultados
 
-if count == nil?
-elsif count == 1
-  @limit = 50
-elsif count == 2
-  @limit = 25
-elsif count == 3
-  @limit = 20
-end
+# @tastes.split.each do |taste|
+#   url = URI("https://api.foursquare.com/v3/places/search?query=#{taste}&ll=#{@latlong}&radius=#{@radius}&categories=#{@categories}&exclude_all_chains=#{@exclude_chains}&fields=name%2Cgeocodes%2Cdistance%2Cdescription%2Ctel%2Cwebsite%2Csocial_media%2Crating%2Cprice%2Ctastes%2Clocation&min_price=#{@min_price}&max_price=#{@max_price}&open_now=#{@open_now}&limit=#{@limit}")
+#   foursquare_request = URI.open(url, "Authorization" => ENV['FOURSQUARE_KEY']).read
+#   foursquare_response = JSON.parse(foursquare_request)
+#   @resultados_organizados = foursquare_response['results']
+# end
 
-@resultados_organizados = [] # 60 resultados
+# @resultados_organizados.select { |r| r["tastes"].include?(@query) }
 
-@tastes.split.each do |taste|
-  url = URI("https://api.foursquare.com/v3/places/search?query=#{taste}&ll=#{@latlong}&radius=#{@radius}&categories=#{@categories}&exclude_all_chains=#{@exclude_chains}&fields=name%2Cgeocodes%2Cdistance%2Cdescription%2Ctel%2Cwebsite%2Csocial_media%2Crating%2Cprice%2Ctastes%2Clocation&min_price=#{@min_price}&max_price=#{@max_price}&open_now=#{@open_now}&limit=#{@limit}")
-  foursquare_request = URI.open(url, "Authorization" => ENV['FOURSQUARE_KEY']).read
-  foursquare_response = JSON.parse(foursquare_request)
-  @resultados_organizados = foursquare_response['results']
-end
-
-@resultados_organizados.select { |r| r["tastes"].include?(@query) }
-
-@resultados_organizados.sample
+# @resultados_organizados.sample
