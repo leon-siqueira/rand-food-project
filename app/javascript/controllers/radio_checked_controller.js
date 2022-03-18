deafc013a624e6691138448e8aca108ea85c16e7
leon-siqueira <leon.siqueir4@gmail.com>
2022-03-18 13:12:12 -0300

    pill buttons done, home container 'refactored' w/ bootstrap

--- app/javascript/controllers/radio_checked_controller.js
+++ app/javascript/controllers/radio_checked_controller.js
@@ -3,7 +3,18 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
 
+  static targets = ["option"]
+
   connect() {
     console.log("Hello from our first Stimulus controller")
+  }
+
+  selectRadioOption(e){
+    console.log(this.optionTargets)
+    this.optionTargets.forEach(option => {
+      console.log(option);
+      option.classList.remove("radio-checked");
+    });
+    e.currentTarget.classList.add("radio-checked");
   }
 }
