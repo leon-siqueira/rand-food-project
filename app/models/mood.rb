72d7a096a39f6dad171814e2a8cf9858d6803fd1
c <caio.asevedo@gmail.com>
2022-03-20 21:36:15 -0300

    form query added

--- app/models/mood.rb
+++ app/models/mood.rb
@@ -2,5 +2,5 @@ class Mood < ApplicationRecord
   belongs_to :user, optional: true
 
   TASTES = %i[sushi pizza burger healthy]
-  # QUERY = %i[music romantic crowded]
+  QUERY = ['romantic', 'good for groups', 'music', 'crowded']
 end
