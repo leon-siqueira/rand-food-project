9f81b7ac35fe8d262417388c2b1b30057b341d26
Maressa Alves <maressa.msoa@gmail.com>
2022-03-21 12:43:54 -0300

    basico da pagina finalizado

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,11 @@ ActiveRecord::Schema.define(version: 2022_03_18_175900) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "cities", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "moods", force: :cascade do |t|
     t.string "name", default: "", null: false
     t.string "query", default: "", null: false
@@ -26,6 +31,14 @@ ActiveRecord::Schema.define(version: 2022_03_18_175900) do
     t.datetime "updated_at", precision: 6, null: false
     t.text "tastes", default: [], array: true
     t.index ["user_id"], name: "index_moods_on_user_id"
+  end
+
+  create_table "searches", force: :cascade do |t|
+    t.string "address"
+    t.float "latitude"
+    t.float "longitude"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
