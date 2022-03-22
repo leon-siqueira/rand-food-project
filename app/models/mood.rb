778dcc04645451dafaa413d5e16e3a5102989af2
c <caio.asevedo@gmail.com>
2022-03-21 22:46:46 -0300

    mood index new

--- app/models/mood.rb
+++ app/models/mood.rb
@@ -1,6 +1,6 @@
 class Mood < ApplicationRecord
   belongs_to :user, optional: true
 
-  TASTES = %i[Argentinian Asian Brazilian BBQ Burger Chinese Comfy Coffee Desserts French Italian Mexican Peruvian Pizza Seafood Sushi Wine Vegan]
-  QUERY = ['romantic', 'good for groups', 'music', 'crowded']
+  TASTES = %i[Argentinian Asian BBQ Brazilian Burger Comfy Chinese Coffee Desserts French Italian Mexican Peruvian Pizza Seafood Sushi Wine Vegan]
+  QUERY = ['romantic', 'good for groups', 'music', 'crowded', 'family-friendly', 'fancy', 'good for dates', 'late night']
 end
